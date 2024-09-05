7dc3595885a4a00c8f632d7f9a05105bb05fab3c
Steve2404 <jatsasteve@gmail.com>
2024-09-05 21:17:16 +0200

    closes #8; done

--- Backend/src/main/java/fr/afrogeek/geekhrconnect/GeekhrconnectApplication.java
+++ Backend/src/main/java/fr/afrogeek/geekhrconnect/GeekhrconnectApplication.java
@@ -6,10 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GeekhrconnectApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(GeekhrconnectApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Bienvenue sur notre Applications Spring ";
     }
 
 }
